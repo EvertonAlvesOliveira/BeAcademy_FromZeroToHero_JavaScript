57190ad12c5ca153b8b486052408a81023db8b5c
Everton Alves Oliveira <everton_si@outlook.com>
2022-11-23 17:50:15 -0300

    1 - Introdução JavaScript Intermediário - Introd. Variáveis

---
+++ introd-variaveis-interm.js
@@ -0,0 +1,31 @@
+/* Scope */
+
+{
+    let x = 2;
+    //console.log(x)
+}
+
+{
+    var y = 12;
+}
+//console.log(y)
+
+
+/* Reassing a value */
+let w = 10
+ w = 5
+//console.log(w)
+
+
+/* Template String */
+
+let firstName = "José";
+let lastName = "da Silva";
+
+let text =`Welcome ${firstName}, ${lastName}!`;
+//console.log(text)
+
+let price = 30;
+let fees = 0.25;
+let total = `O total do produto é: R$ ${(price + fees)}`;
+console.log(total)
